ca459728992baa8df66c7f7383db865ab1ad8acd
Jordi Prats <jprats@systemadmin.es>
2017-02-16 18:16:27 +0100

    afegida instancia i db pel catalog

--- manifests/dir/catalog.pp
+++ manifests/dir/catalog.pp
@@ -3,10 +3,25 @@ define bacula::dir::catalog (
                               $catalog_name = $name,
                               $dbname       = 'bacula',
                               $dbuser       = 'bacula',
+                              $setup_mysql  = true,
                             ) {
   concat::fragment{ "/etc/bacula/bacula-dir.conf catalog ${catalog_name}":
     target  => '/etc/bacula/bacula-dir.conf',
     order   => '10',
     content => template("${module_name}/dir/catalog.erb"),
+  }
+
+  if($setup_mysql)
+  {
+    mysql::community::instance { 'bacula':
+      port              => '3306',
+      password          => $dbpassword,
+      add_default_mycnf => true,
+      default_instance  => true,
+    }
+
+    mysql_database { $dbname:
+      ensure => 'present',
+    }
   }
 }
