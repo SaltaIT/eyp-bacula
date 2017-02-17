2339439f2f34001000b51b7f9cd36b8ff1cb0303
Jordi Prats <jprats@systemadmin.es>
2017-02-17 17:43:04 +0100

    afegits directoris per la config del director

--- manifests/dir/config.pp
+++ manifests/dir/config.pp
@@ -12,6 +12,69 @@ class bacula::dir::config inherits bacula::dir {
     mode    => '0640',
   }
 
+  file { '/etc/bacula/bacula-dir':
+    ensure => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+  }
+
+  file { '/etc/bacula/bacula-dir/jobtemplates':
+    ensure => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    require => File['/etc/bacula/bacula-dir'],
+  }
+
+  file { '/etc/bacula/bacula-dir/jobs':
+    ensure => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    require => File['/etc/bacula/bacula-dir'],
+  }
+
+  file { '/etc/bacula/bacula-dir/filesets':
+    ensure => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    require => File['/etc/bacula/bacula-dir'],
+  }
+
+  file { '/etc/bacula/bacula-dir/schedules':
+    ensure => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    require => File['/etc/bacula/bacula-dir'],
+  }
+
+  file { '/etc/bacula/bacula-dir/storages':
+    ensure => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    require => File['/etc/bacula/bacula-dir'],
+  }
+
+  file { '/etc/bacula/bacula-dir/clients':
+    ensure => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    require => File['/etc/bacula/bacula-dir'],
+  }
+
+  file { '/etc/bacula/bacula-dir/pools':
+    ensure => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    require => File['/etc/bacula/bacula-dir'],
+  }
+
   concat::fragment{ '/etc/bacula/bacula-dir.conf base conf':
     target  => '/etc/bacula/bacula-dir.conf',
     order   => '00',
