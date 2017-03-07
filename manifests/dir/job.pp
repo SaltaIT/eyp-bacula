24574c17281eebb63431f5030ffadf2962990ced
Jordi Prats <jprats@systemadmin.es>
2017-03-07 16:53:00 +0100

    job

---
+++ manifests/dir/job.pp
@@ -0,0 +1,27 @@
+define bacula::dir::job (
+                          $job_name       = $name,
+                          $type           = 'Backup',
+                          $level          = undef,
+                          $client         = undef,
+                          $fileset        = undef,
+                          $schedule       = undef,
+                          $storage        = undef,
+                          $pool           = undef,
+                          $spool_data     = false,
+                          $priority       = undef,
+                          $run_before_job = undef,
+                          $run_after_job  = undef,
+                          $write_bootstrap = '/var/lib/bacula/%c.bsr',
+                        ) {
+  $job_type = 'Job'
+
+  $job_name_filename=downcase($job_name)
+
+  file { "/etc/bacula/bacula-dir/jobs/${job_name_filename}.conf":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template("${module_name}/dir/job.erb"),
+  }
+}
--- manifests/dir/jobtemplate.pp
+++ manifests/dir/jobtemplate.pp
@@ -18,17 +18,29 @@
 # RunAfterJob  = "/etc/bacula/scripts/delete_catalog_backup"
 
 define bacula::dir::jobtemplate (
-                                  $job_name        = $name,
-                                  $type            = 'Backup',
-                                  $level           = undef,
-                                  $client          = undef,
-                                  $fileset         = undef,
-                                  $schedule        = undef,
-                                  $storage         = undef,
-                                  $pool            = undef,
-                                  $spool_data      = false,
-                                  $priority        = undef,
+                                  $job_name       = $name,
+                                  $type           = 'Backup',
+                                  $level          = undef,
+                                  $client         = undef,
+                                  $fileset        = undef,
+                                  $schedule       = undef,
+                                  $storage        = undef,
+                                  $pool           = undef,
+                                  $spool_data     = false,
+                                  $priority       = undef,
+                                  $run_before_job = undef,
+                                  $run_after_job  = undef,
                                   $write_bootstrap = '/var/lib/bacula/%c.bsr',
                                 ) {
   $job_type = 'JobDefs'
+
+  $job_name_filename=downcase($job_name)
+
+  file { "/etc/bacula/bacula-dir/jobtemplates/${job_name_filename}.conf":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template("${module_name}/dir/job.erb"),
+  }
 }
