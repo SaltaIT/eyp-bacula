d28dde8c1cf9311b3d07fb74082de51803b8092e
Jordi Prats <jprats@systemadmin.es>
2017-03-08 17:30:35 +0100

    init DB script

--- manifests/dir/install.pp
+++ manifests/dir/install.pp
@@ -4,8 +4,39 @@ class bacula::dir::install inherits bacula::dir {
   {
     package { $bacula::params::package_dir_name:
       ensure => $bacula::dir::package_ensure,
-      before => File['/var/lib/bacula'],
+      before => File[ [ '/var/lib/bacula', '/etc/bacula/scripts' ] ],
     }
+  }
+
+  # root@ubuntu16:/etc/bacula/scripts# ls -ld /etc/bacula
+  # drwxr-xr-x 4 root root 4096 Mar  8 17:10 /etc/bacula
+
+  file { '/etc/bacula':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  # root@ubuntu16:/etc/bacula/scripts# ls -ld /etc/bacula/scripts/
+  # drwxr-xr-x 2 root root 4096 Mar  7 17:29 /etc/bacula/scripts/
+  # root@ubuntu16:/etc/bacula/scripts#
+
+  file { '/etc/bacula/scripts':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File['/etc/bacula'],
+  }
+
+  file { '/etc/bacula/scripts/mktables_70.sql':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => "puppet:///modules/${module_name}/mktables_70.sql",
+    require => File['/etc/bacula/scripts'],
   }
 
   file { '/var/lib/bacula':
