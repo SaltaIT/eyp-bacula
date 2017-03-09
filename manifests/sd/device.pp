80c9744bd212ba26a18280231271ad75acc24c03
Jordi Prats <jprats@systemadmin.es>
2017-03-09 16:30:59 +0100

    afegit mkdir al sd_device

--- manifests/sd/device.pp
+++ manifests/sd/device.pp
@@ -13,21 +13,38 @@
 #
 define bacula::sd::device (
                             $archive_device,
-                            $device_name         = $name,
-                            $description         = undef,
-                            $max_concurrent_jobs = '20',
-                            $media_type          = "File-${::fqdn}",
-                            $device_type         = 'File',
-                            $label_media         = true,
-                            $random_access       = true,
-                            $automatic_mount     = true,
-                            $removable_media     = false,
-                            $always_open         = false,
-                            $max_concurrent_jobs = '5',
+                            $mkdir_archive_device = true,
+                            $device_name          = $name,
+                            $description          = undef,
+                            $max_concurrent_jobs  = '20',
+                            $media_type           = "File-${::fqdn}",
+                            $device_type          = 'File',
+                            $label_media          = true,
+                            $random_access        = true,
+                            $automatic_mount      = true,
+                            $removable_media      = false,
+                            $always_open          = false,
+                            $max_concurrent_jobs  = '5',
                           ) {
   concat::fragment{ "/etc/bacula/bacula-sd.conf device ${device_name}":
     target  => '/etc/bacula/bacula-sd.conf',
     order   => '10',
     content => template("${module_name}/sd/device.erb"),
+  }
+
+  if($mkdir_archive_device)
+  {
+    exec { "mkdir -p archive_device ${archive_device}":
+      command => "mkdir -p ${archive_device}",
+      creates => $archive_device,
+    }
+
+    file { $archive_device:
+      ensure => 'directory',
+      owner => 'bacula',
+      group => 'bacula',
+      mode => '0750',
+      require => Exec["mkdir -p archive_device ${archive_device}"],
+    }
   }
 }
