cd14c95473c46f9e4a6349441d664f2e16178632
Jordi Prats <jprats@systemadmin.es>
2017-03-09 14:22:47 +0100

    wait for pid

--- files/wait-for-bacula-pid.sh
+++ files/wait-for-bacula-pid.sh
@@ -3,12 +3,14 @@
 BACULATIMEOUT=60
 BACULAPORT=9101
 BACULARUN=/var/run/bacula
+BACULADAEMON=dir
 
-while getopts 't:p:' OPT; do
+while getopts 't:p:r:d:' OPT; do
   case $OPT in
     t)  BACULATIMEOUT=$OPTARG;;
     p)  BACULAPORT=$OPTARG;;
     r)  BACULARUN=$OPTARG;;
+    d)  BACULADAEMON=$OPTARG;;
     *)  JELP="yes";;
   esac
 done
@@ -21,9 +23,9 @@ fi
 
 for i in $(seq 1 ${BACULATIMEOUT});
 do
-  if [ -f "${BACULARUN}/bacula-dir.${BACULAPORT}.pid" ];
+  if [ -f "${BACULARUN}/bacula-${BACULADAEMON}.${BACULAPORT}.pid" ];
   then
-    echo "found ${BACULARUN}/bacula-dir.${BACULAPORT}.pid - iteration $i"
+    echo "found ${BACULARUN}/bacula-${BACULADAEMON}.${BACULAPORT}.pid - iteration $i"
     exit 0
   fi
   sleep 1
