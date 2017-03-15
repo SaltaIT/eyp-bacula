3c281b7638d37b7b7f48226dc8de6741d1b8b3df
Jordi Prats <jprats@systemadmin.es>
2017-03-15 14:00:29 +0100

    lint

--- manifests/dir/config.pp
+++ manifests/dir/config.pp
@@ -11,7 +11,7 @@ class bacula::dir::config inherits bacula::dir {
     # /usr/sbin/bacula-dir -c /etc/bacula/bacula-dir.conf -u bacula -g bacula
     # /var/run/bacula/bacula-dir.9101.pid
     systemd::service { 'bacula-director':
-      execstart       => inline_template("/usr/sbin/bacula-dir -c /etc/bacula/bacula-dir.conf -u bacula -g bacula<% if defined?(@debug_level) %> -d <%= @debug_level %><% end %>"),
+      execstart       => inline_template('/usr/sbin/bacula-dir -c /etc/bacula/bacula-dir.conf -u bacula -g bacula<% if defined?(@debug_level) %> -d <%= @debug_level %><% end %>'),
       pid_file        => "/var/run/bacula/bacula-dir.${bacula::dir::port}.pid",
       execstartpost   => '/etc/bacula/scripts/wait-for-bacula-pid.sh -d dir',
       type            => 'forking',
--- manifests/fd/config.pp
+++ manifests/fd/config.pp
@@ -7,7 +7,7 @@ class bacula::fd::config inherits bacula::fd {
   if($bacula::params::systemd)
   {
     systemd::service { 'bacula-fd':
-      execstart       => inline_template("/usr/sbin/bacula-fd -c /etc/bacula/bacula-fd.conf<% if defined?(@debug_level) %> -d <%= @debug_level %><% end %>"),
+      execstart       => inline_template('/usr/sbin/bacula-fd -c /etc/bacula/bacula-fd.conf<% if defined?(@debug_level) %> -d <%= @debug_level %><% end %>'),
       pid_file        => "/var/run/bacula/bacula-fd.${bacula::fd::port}.pid",
       type            => 'forking',
       timeoutstartsec => '1m',
@@ -31,7 +31,7 @@ class bacula::fd::config inherits bacula::fd {
   {
     bacula::fd::director { $bacula::fd::director_name:
       password    => $bacula::fd::director_password,
-      description => "default director",
+      description => 'default director',
     }
   }
 }
